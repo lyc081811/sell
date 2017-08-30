4d552a81aeeb5bf0622a6cca5de47f9ac28b57dd
lyc <269632358@qq.com>
2017-08-30 23:31:36 +0800

    增加router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
-  <div id="app">
+  <div>
     <v-header></v-header>
     <div class="tab">
-      i am tab
+      <div class="tab-item"><a v-link="{path: '/goods'}">商品</a></div>
+      <div class="tab-item"><a v-link="{path: '/ratings'}">评论</a></div>
+      <div class="tab-item"><a v-link="{path: '/seller'}">商家</a></div>
     </div>
-    <div class="content">
-      i am content
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 <script>
 import header from './components/header/header.vue';
 export default {
-  component: {
-    v-header: header
+  components: {
+    'v-header': header
   }
 };
 </script>
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+.tab
+  display: flex
+  width: 100%
+  height: 40px
+  line-height: 40px
+  border-bottom: 1px solid #ccc
+  .tab-item
+    flex: 1
+    text-align: center
+    & > a
+      display: block
+      font-size: 14px
+      color: rgb(77,85,93)
+      &.active
+        color: rgb(240,20,20)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue';
 import App from './App';
+import goods from 'components/goods/goods';
+import seller from 'components/seller/seller';
+import ratings from 'components/ratings/ratings';
+import VueRouter from 'vue-router';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter);
+let app = Vue.extend(App);
+let router = new VueRouter({
+  linkActiveClass: 'active'
 });
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/seller': {
+    component: seller
+  },
+  '/ratings': {
+    component: ratings
+  }
+});
+router.start(app, '#app');
+router.go('/goods');
