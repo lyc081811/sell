5fb7ee2474987d1212f873177de641d7671099fb
lycorz <269632358@qq.com>
2017-08-31 23:46:54 +0800

    添加header样式

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,7 +13,7 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div class="support" v-if="seller.supports">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
@@ -28,22 +28,28 @@
       seller: {
         type: Object
       }
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
   };
 </script>
 
 <style  lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
   .header
     color #fff
-    background-color #000
+    background-color #999
     .content-wrapper
       padding 24px 12px 18px 24px
       font-size 0
       .avatar
         display inline-block
+        vertical-align top
+        img
+          border-radius 2px
       .content
         display inline-block
-        font-size 14px
         margin-left 16px
         .title
           margin 2px 0 8px 0
@@ -51,5 +57,40 @@
             display inline-block
             width 30px
             height 18px
-            background 
+            bg-image('brand')
+            background-size 30px 18px
+            background-repeat no-repeat
+            vertical-align top
+          .name
+            font-size 16px
+            margin-left 6px
+            line-height 18px
+            font-weight bold
+        .description
+          margin-bottom 10px
+          line-height 12px
+          font-size 12px
+        .support
+          .icon
+            display inline-block
+            vertical-align top
+            width 12px
+            height 12px
+            margin-right 4px
+            background-size 12px 12px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height 12px
+            font-size 10px
+
 </style>
