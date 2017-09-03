257eeb9d7b5aff9c41622a9b0a9abff2f514f9c4
lyc081811 <269632358@qq.com>
2017-09-03 23:01:03 +0800

    添加stars

---
+++ src/components/star/star.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="start" :class="starType">
+        <span v-for="itemClass in itemClasses" :class="itemClasses"  class="start-item"></span>
+    </div>
+</template>
+
+<script>
+  export default {
+      props: {
+          size: {
+              type: Number
+          },
+          store: {
+              type: Number
+          }
+      },
+      computed: {
+          starType() {
+              return 'star-'+this.size
+          }
+      }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    .star
+         .star-item
+            display block
+            background-repeat no-repeat
+        &.star-48
+            .star-item
+                width 20px
+                height 20px
+                
+        &.star-36
+        &.star-24
+       
+</style>
