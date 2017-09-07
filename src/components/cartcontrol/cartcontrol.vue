574561b2fe18ba34ce9851e572d0988ee3e49a1e
lyc081811 <269632358@qq.com>
2017-09-07 23:17:44 +0800

    底部组件补充

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    props: {
+        food: {
+            type: Object
+        }
+    }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    
+</style>
--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,20 +3,80 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight': totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">4353</div>
-        <div class="desc">另需配送费元</div>
+        <div class="price" :class="{'highlight': totalCount>0}">{{totalPrice}}元</div>
+        <div class="desc">另需配送费{{deliveryPrice}}元</div>
       </div>
-      <div class="content-right"></div>
+      <div class="content-right">
+        <div class="pay" :class="payClass">
+          {{payDesc}}
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {};
+  export default {
+    props: {
+      deliveryPrice: {
+        type: Number,
+        default: 0
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      },
+      selectFoods: {
+        type: Array,
+        default() {
+          return [
+            {
+              price: 10,
+              count: 19
+            }
+          ];
+        }
+      }
+    },
+    computed: {
+      totalPrice() {
+        let total = 0;
+        this.selectFoods.forEach(food => {
+          total += food.price * food.count;
+        });
+        return total;
+      },
+      totalCount() {
+        let count = 0;
+        this.selectFoods.forEach(food => {
+          count += food.count;
+        });
+        return count;
+      },
+      payDesc() {
+        if (this.totalPrice === 0) {
+          return `￥${this.minPrice}元起送`;
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice;
+          return `还差￥${diff}元起送`;
+        } else {
+          return '去结算';
+        }
+      },
+      payClass() {
+        if (this.totalPrice < this.minPrice) {
+          return 'not-enough';
+        } else {
+          return 'enough';
+        }
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -46,16 +106,34 @@
           vertical-align top
           border-radius 50%
           background-color #141d27
+          .num
+            position absolute
+            top 0
+            right 0
+            width 24px
+            height 16px
+            line-height 16px
+            text-align center
+            border-radius 16px
+            font-size 9px
+            font-weight 400
+            color #fff
+            background-color rgb(240,20,20)
+            box-box-shadow 0 4px 8px rgba(0,0,0,.4)
           .logo
             width 100%
             height 100%
             border-radius 50%
             background-color #2b343c
             text-align center
+            &.highlight
+              background rgb(0,160,220)
             .icon-shopping_cart
               font-size 24px
               color #80858a
-              line-height 44px 
+              line-height 44px
+              &.highlight
+                color #fff
         .price
           display inline-block
           font-size 16px
@@ -66,7 +144,9 @@
           margin-top 12px
           box-sizing border-box
           padding-right 12px
-          border-right 1px solid rgba(255,255,255,.1) 
+          border-right 1px solid rgba(255,255,255,.1)
+          &.highlight
+            color #fff
         .desc
           display inline-block
           vertical-align top
@@ -78,5 +158,18 @@
       .content-right
         flex 0 0 105px
         width 105px
-
+        .pay
+          line-height 48px
+          height 48px
+          text-align center
+          font-size 12px
+          color rgba(255,255,255,.4)
+          font-weight 700
+          background-color #2b333b
+          &.not-enough
+            background #2b333b
+          &.enough
+            background #00b43c
+            color #fff
+          
 </style>
